f38c8428fa23de373cd859e092b242bf978f4b64
Christophe Willemsen <willemsen.christophe@gmail.com>
2017-05-09 21:33:55 +0200

    added local .env loader

--- web/index.php
+++ web/index.php
@@ -2,6 +2,8 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
+$dotenv = new \Dotenv\Dotenv(__DIR__.'/..');
+$dotenv->load();
 $app = new Silex\Application();
 
 // Set up Twig Templating Engine
